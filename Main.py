57d57137a14b89eabbab46859ce47af948f2bcef
tomasAudejaitis <163133492+tomasAudejaitis@users.noreply.github.com>
2025-08-11 09:29:19 -0700

    Update Main.py
    updating it for the new frontend I added today and yesterday

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import os
 from Gather import Gather
 from analyzer import RepoAnalyzer
+from Frontend import Frontend
 
 if __name__ == "__main__":
     # Load GitHub token from environment variable
@@ -13,11 +14,17 @@ if __name__ == "__main__":
         gather.save_to_file('data.json')
         gather.scrape_contributors()
         gather.save_contributors_to_file("contributorData.json")
-    #    gather.save_contributors_to_file('contributorData.json')
+ 
         
         # Create RepoAnalyzer instance and execute analysis
         analyzer = RepoAnalyzer(filename='data.json')
         analyzer.execute()
-        analyzer.percentageByCategory('language', 'commits')
+        commit_data = analyzer.percentageByCategory('language','commits')
+
+        frontend= Frontend()
+        frontend.plotLanguageCommitPie(commit_data)
+        frontend.plotRepoBarCharts(analyzer, top_n=10)
     else:
         print("Failed to scrape trending repositories")
+
+        
